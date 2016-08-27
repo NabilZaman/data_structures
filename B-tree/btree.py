de07e0c6b3e8adc21058d2f629f3cd9a805704a2
Nabil Zaman <nabilzaman1@gmail.com>
2016-08-26 21:06:16 -0700

    Stubbed out most of the exposed tree operations.
    Still not sure what I'm doing in terms of implementing them.

--- B-tree/btree.py
+++ B-tree/btree.py
@@ -9,12 +9,50 @@ https://en.wikipedia.org/wiki/B-tree.
 class BTree(object):
     """ The BTree class is a 'generic' implementation of a BTree.
         The tree can contain any comparable types.
-        The tree is instantiated with a "order" or "branching factor"
+        The tree is instantiated with an "order" or "branching factor"
         which specifies the maximum number of children any node in the
         tree may have. """
 
-    def __init__(self, order):
+    def __init__(self, order, is_root=True):
         self.order = order
+        self.keys = []
+        self.children = []
+
+
+    def contains(self, key):
+        """ Return whether the tree contains the given key. """
+        return False
+
+    def insert(self, key):
+        """
+        Insert given key into key. Return whether the tree was modified.
+
+        The tree will not be modified if the key was already present.
+        """
+
+        return False
+
+    def delete(self, key):
+        """
+        Remove given key from the tree. Return whether tree was modified.
+
+        The tree will not be modified if the key was not present.
+        """
+
+        return False
+
+    def height(self):
+        """ Return the height of the tree. """
+
+        return 0
+
+    def is_leaf(self):
+        """ Returns whether the tree is a leaf node (has no children)."""
+        return not self.children
+
+    def is_empty(self):
+        """ Returns whether the tree is empty (has no keys). """
+        return not self.keys
 
     # More to come?
 
