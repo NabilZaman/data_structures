75302200d7bb176085c928dea7c427c6762887a6
Nabil Zaman <nabilzaman1@gmail.com>
2016-08-31 02:48:00 -0700

    Fixed insertion bugs. Implemented delete (broken).

--- B-tree/btree.py
+++ B-tree/btree.py
@@ -32,19 +32,31 @@ class BTree(object):
         which specifies the maximum number of children any node in the
         tree may have. """
 
-    def __init__(self, order, parent=None, keys=[], children=[]):
-        if order < 1:
-            raise AttributeError("Order must be a positive integer.")
+    def __init__(self, order, parent=None, keys=None, children=None):
+        if order < 3:
+            raise AttributeError("Order must be greater than 2.")
         self.order = order
         self.parent = parent
-        self.keys = keys
-        self.children = children
+        if keys is None:
+            self.keys = []
+        else:
+            self.keys = keys
+        if children is None:
+            self.children = []
+        else:
+            self.children = children
+            for child in self.children:
+                child.parent = self
+
+        self.size = len(self.keys) + sum(len(child) for child in self.children)
 
 
     def contains(self, key):
         """ Return whether the tree contains the given key. """
         if key in self.keys:
             return True
+        elif self.is_leaf:
+            return False
         else:
             return self._find_child_for_key(key).contains(key)
 
@@ -65,10 +77,17 @@ class BTree(object):
             # Then make sure you haven't become too large
             self._split_if_needed()
 
+            self.size += 1
             return True
         else:
             # Recursively insert into appropriate child
-            return self._find_child_for_key(key).insert(key)
+            changed = self._find_child_for_key(key).insert(key)
+            if changed:
+                self.size += 1
+            return changed
+
+    def insert_all(self, keys):
+        return [self.insert(key) for key in keys]
 
     def delete(self, key):
         """
@@ -76,13 +95,46 @@ class BTree(object):
 
         The tree will not be modified if the key was not present.
         """
+        print "Trying to delete", key, "from", self.keys
+        if self.is_leaf():
+            if key not in self.keys:
+                # Nothing to do
+                return False
+            else:
+                # Remove the key and make sure you haven't become too small
+                self.keys.remove(key)
+                self._merge_if_needed()
 
-        return False
+                self.size -= 1
+                return True
+        else:
+            if key not in self.keys:
+                # Recursively try to delete from appropriate child
+                changed = self._find_child_for_key(key).delete(key)
+                if changed:
+                    self.size -= 1
+                return changed
+            else:
+                replacement_index = find_index_in_sorted_list(key, self.keys)
+                # Find a suitable replacement to replace the deleted key
+                replacement_child = self.children[replacement_index]
+                replacement = replacement_child.find_greatest()
+                # Replace the deleted key
+                self.keys[replacement_index] = replacement
+                # Remove the replacement value from child it came from
+                replacement_child.delete(replacement)
+
+                self.size -= 1
+                return True
+
+    def delete_all(self, keys):
+        return [self.delete(key) for key in keys]
 
     def height(self):
         """ Return the height of the tree. """
-
-        return 0
+        if self.is_leaf():
+            return 1
+        return 1 + max(child.height() for child in self.children)
 
     def is_leaf(self):
         """ Return whether the tree is a leaf node (has no children). """
@@ -100,6 +152,95 @@ class BTree(object):
         """ Return whether you have exceeded capacity. """
         return len(self.keys) >= self.order
 
+    def is_too_empty(self):
+        """
+        Return whether you have dropped below needed capacity.
+
+        Does not apply to the root node.
+        """
+        return len(self.keys) < self.order/2 and not self.is_root()
+
+    def find_greatest(self):
+        """ Return the greatest key value in the tree. """
+        if self.is_leaf():
+            return self.keys[-1]
+        else:
+            return self.children[-1].find_greatest()
+
+    def find_least(self):
+        """ Return the least key value in the tree. """
+        if self.is_leaf():
+            return self.keys[0]
+        else:
+            return self.children[0].find_least()
+
+    def _merge_if_needed(self):
+        """
+        Helper function. Merge this node if it is too small.
+
+        Do nothing if the node is not below needed capacity.
+        """
+        print "Merging if necessary:", self.is_too_empty()
+        if self.is_too_empty():
+            self.parent._merge_child_with_key(self.keys[0])
+
+    def _merge_child_with_key(self, key):
+        """
+        Helper function. Increase the size of the child with given key.
+
+        There are two strategies for doing this.
+            Firstly you may find a find a sibling with spare keys and
+            rotate that siblings extreme key closest to the problem node
+            so that that key becomes the new seperator between them and
+            the old seperator is inserted into the problem node.
+            Secondly if no such sibling can be find, combine this node
+            and one of its siblings (that are minimal in size) and add the
+            key seperating them to create a new, full-sized node.
+        """
+        child_index = find_index_in_sorted_list(key, self.keys)
+        child = self.children[child_index]
+        # First check if there is a child to its right that can donate a key
+        if child_index + 1 < len(self.children) and \
+            len(self.children[child_index+1].keys) > self.order/2:
+            right_child = self.children[child_index+1]
+            # Rotate "to the left"
+            child.keys.append(self.keys[child_index])
+            self.keys[child_index] = right_child.keys[0]
+            right_child.delete(right_child.keys[0])
+        elif child_index - 1 >= 0 and \
+            len(self.children[child_index-1].keys) > self.order/2:
+            left_child = self.children[child_index-1]
+            # Rotate "to the right"
+            self.child.keys.append(self.keys[child_index-1])
+            self.keys[child_index-1] = left_child.keys[-1]
+            left_child.delete(left_child.keys[-1])
+        else:
+            if child_index + 1 < len(self.children):
+                # Merge with the right_child
+                left_child_index = child_index
+            else:
+                # Merge with the left child
+                left_child_index = child_index-1
+
+            right_child_index = left_child_index+1
+            left_child = self.children[left_child_index]
+            right_child = self.children[right_child_index]
+            print "Merging...", left_child.keys, right_child.keys
+            # Combine the keys and children of the two children you're merging
+            newKeys = left_child.keys + [self.keys.pop(left_child_index)] + \
+                right_child.keys
+            newChildren = left_child.children + right_child.children
+
+            # Add the new merged child to your list of children
+            mergedChild = BTree(self.order, parent=self, keys=newKeys,\
+                children=newChildren)
+            self.children = self.children[:left_child_index] +\
+                [mergedChild] + self.children[right_child_index+1:]
+
+            # Merge yourself if this caused you to grow too small
+            self._merge_if_needed()
+
+
     def _split_if_needed(self):
         """
         Helper function. Split this node if it is too large.
@@ -116,9 +257,9 @@ class BTree(object):
             if self.is_root():
                 # Need to split yourself
                 dummy_child = BTree(self.order, parent=self,\
-                    keys=self.keys, children=[])
+                    keys=self.keys, children=self.children)
                 self.keys = []
-                self.children.append(dummy_child)
+                self.children = [dummy_child]
 
                 self._split_child_with_key(median)
 
@@ -174,21 +315,25 @@ class BTree(object):
         return self.children[child_index]
 
 
-    def __str__(self, prefix=''):
+    def __repr__(self, level=0):
         result = ''
         if self.is_leaf():
-            for index, key in enumerate(self.keys):
-                result += '{pre}{key}:\n'.format(pre=prefix, key=key)
+            result += '{pre}{keys}\n'.format(pre=level*'\t', keys=self.keys)
 
         else:
             for index, key in enumerate(self.keys):
-                result += '{pre} {child}\n'.format(pre=prefix, \
-                    child=self.children[index].__str__(prefix + '\t'))
-                result += '{pre} {key}:\n'.format(pre=prefix, key=key)
-            result += '{pre} {child}\n'.format(pre=prefix, \
-                child=self.children[-1].__str__(prefix + '\t'))
+                result += '{pre}{child}\n'.format(pre=level*'\t', \
+                    child=self.children[index].__repr__(level+1))
+                result += '{pre}{key}:\n'.format(pre=level*'\t', key=key)
+            result += '{pre}{child}\n'.format(pre=level*'\t', \
+                child=self.children[-1].__repr__(level+1))
         return result
-        # More to come?
+
+    def __len__(self):
+        """ Return number of keys in tree. """
+        return len(self.keys) + sum(len(child) for child in self.children)
+
+    # More to come?
 
 if __name__ == '__main__':
     test_list1 = [1, 3, 5, 7, 9, 11, 13]
@@ -201,5 +346,11 @@ if __name__ == '__main__':
     print [(find_index_in_sorted_list(key, test_list3), key) for key in keys]
 
     tree = BTree(5)
-    [tree.insert(val) for val in range(40, 140, 2)]
+    tree.insert_all(range(100))
+    tree.delete_all(range(36))
+    print len(tree)
+    print tree.size
     print tree
+
+    print tree.find_greatest()
+    print tree.find_least()
