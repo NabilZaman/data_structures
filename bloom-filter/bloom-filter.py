11d9438235b6aa3573ca620eb1e0b4397f99a0c6
Nabil Zaman <nabilzaman1@gmail.com>
2016-09-18 22:26:55 -0700

    Initial commit for bloom filter.

---
+++ bloom-filter/bloom-filter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#!/usr/bin/env python
+"""
+bloom-filter.py
+
+This file contains an implementation of a bloom filter as outlined in
+https://en.wikipedia.org/wiki/Bloom_filter
+"""
+
